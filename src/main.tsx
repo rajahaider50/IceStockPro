e97ce690dcd7d8df5cf7fe14d5f48b74262dccd7
ff9051 <rajahaider9053@gmail.com>
2026-08-25 16:20:11 +0500

    remove: tutorial system - user requested removal

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { TutorialProvider } from './tutorial/TutorialContext'
-import { ALL_TUTORIAL_STEPS } from './tutorial/tutorialSteps'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TutorialProvider allSteps={ALL_TUTORIAL_STEPS}>
-      <App />
-    </TutorialProvider>
+    <App />
   </StrictMode>,
 )
