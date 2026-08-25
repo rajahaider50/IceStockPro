e90a7dae945ed3aa1115308e35b23889abcd7080
ff9051 <rajahaider9053@gmail.com>
2026-08-25 16:13:32 +0500

    feat: integrate tutorial system + add target IDs

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { TutorialProvider } from './tutorial/TutorialContext'
+import { ALL_TUTORIAL_STEPS } from './tutorial/tutorialSteps'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <TutorialProvider allSteps={ALL_TUTORIAL_STEPS}>
+      <App />
+    </TutorialProvider>
   </StrictMode>,
 )
